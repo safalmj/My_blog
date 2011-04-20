b2c1abc994652af004f199bb4be97d40b1506b43
Safal <safal@sproutify.com>
2011-04-20 14:09:48 +0545

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ghub::Application.config.secret_token = '866a32c0a774fc9ddc9e3df430e0aacb868f27140a27beffa929ce8d9be8c332872c67b3045c36f14d9f53ef90276d1272baccc4da681dd5c15e98b477e5464a'
